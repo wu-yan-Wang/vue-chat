4168c8fbb9fd973f3307aef8981472ee13017539
王国栋 <wangguodong194@163.com>
2020-01-15 17:07:59 +0800

    提交.

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,29 @@
+const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV)
+
+const plugins = []
+// 打包时清除console
+if (IS_PROD) {
+  plugins.push('transform-remove-console')
+}
+
+// 按需引入antd
+plugins.push(['import', {
+  'libraryName': 'ant-design-vue',
+  'libraryDirectory': 'es',
+  'style': true // `style: true` 会加载 less 文件
+}])
+
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ], plugins: [
+    '@vue/cli-plugin-babel/preset',
     [
-      'import',
+      '@babel/preset-env',
       {
-        libraryName: 'ant-design-vue',
-        libraryDirectory: 'es',
-        // 为true 启用less
-        style: true
+        // 引入浏览器不兼容的 polyfill
+        'useBuiltIns': 'entry',
+        'corejs': 3
       }
     ]
-  ]
+  ],
+  plugins
 }
